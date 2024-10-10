906ed5985485177b7140a763c4666783be249f8c
Wasan09090 <Wasanalharbi79@gmail.com>
2024-10-10 23:07:08 +0300

    Add files via upload

---
+++ swdexam.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package swd;
+
+/**
+ *
+ * name; wasan salman alharbi
+ * id;44510121
+ */
+
+public class swdexam {
+             // This function returns index of element x in arr[]
+
+            static int search(int arr[], int n, int x)
+
+            {
+
+                        for (int i = 0; i < n; i++) {
+
+                                // Return the index of the element if the element
+
+                                    // is found
+
+                                    if (arr[i] == x)
+
+                                                return i;
+
+                        }
+
+ 
+
+                        // return -1 if the element is not found
+
+                        return -1;
+
+            }
+
+ 
+
+            public static void main(String[] args)
+
+            {
+
+                        int[] arr = { 3, 4, 1, 7, 5 };
+
+                        int n = arr.length;
+
+                        int x = 4;
+
+                        int index = search(arr, n, x);
+
+                        if (index == -1)
+
+                          System.out.println("Element is not present in the array");
+
+                        else
+
+                          System.out.println("Element found at position " + index);
+
+            
+}
+}
